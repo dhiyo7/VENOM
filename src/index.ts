d153faf116bc0066a3f43c1d8b7d7e44496a9cda
dhiyo7 <dhiyoreksa12@gmail.com>
2022-12-25 12:20:18 +0700

    fisrt commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express,{Express, Request, Response} from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+const app: Express = express()
+const port: string = process.env.PORT ?? "3000"
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+    res.send(`Express + Typescript Server`)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port : ${port}`);
+    
+})
