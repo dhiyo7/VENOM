288f35d0d7fa79d88b57a8ad3967b84aec8bb400
dhiyo7 <dhiyoreksa12@gmail.com>
2022-12-25 13:33:50 +0700

    register user routes & logger

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import express,{Express, Request, Response} from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import routes from "./routes";
+import morganMiddleware from "./middlewares/morgan.middleware";
+import Logger from "./utils/logger";
 
-dotenv.config()
-const app: Express = express()
-const port: string = process.env.PORT ?? "3000"
+dotenv.config();
+const app: Express = express();
+const port: string = process.env.PORT ?? "3000";
 
-app.use(express.json())
-app.use(cors())
+app.use(morganMiddleware);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(routes);
 
 app.get("/", (req: Request, res: Response) => {
-    res.send(`Express + Typescript Server`)
-})
+  Logger.info("Checking the API status: Everything is OK");
+  res.send(`Express + Typescript Server`);
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port : ${port}`);
-    
-})
+  Logger.info(`Server running on port : ${port}`);
+});
